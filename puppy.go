632710c8033d0db2006fe0606f621ffd0817ce4d
Lucian Mraz <lucian.mraz@thermofisher.com>
2024-12-01 16:08:22 +0100

    feat: add From11 function

--- puppy.go
+++ puppy.go
@@ -1,6 +1,8 @@
 package puppy
 
 import (
+	"fmt"
+
 	"github.com/vilkovtato/dog"
 )
 
@@ -18,4 +20,8 @@ func BigBark() string {
 
 func BigBarks() string {
 	return dog.WhenGrownUp(Barks())
+}
+
+func From11() {
+	fmt.Println("I'm from version v1.1.0")
 }
